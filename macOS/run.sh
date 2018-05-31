a89784e010b424139f7f585ea321457fc4095a2a
Mark Harris <mharris@technobear.com>
2018-05-31 13:29:53 +0200

    simplfy mother for pi/bela/macos, remote control switch active module, run script for mac

---
+++ macOS/run.sh
@@ -0,0 +1,18 @@
+export PD=${PD:="/Applications/Pd-0.48-1.app/Contents/Resources/bin/pd"}
+cd `dirname $0`
+DIR=`pwd -P`
+export PD_OPTS=${PD_OPTS:="-nogui"}
+
+export MEC_OPTS=${MEC_OPTS:="osckontrol.json"}
+#PD-OPTS='-nogui -rt - audiobuf 4'
+killall pd
+cd ${DIR}/orac
+echo ${PD} ${PD_OPTS} mother/mother.pd main.pd
+${PD} ${PD_OPTS} mother/mother.pd main.pd >/tmp/orac.log 2>&1 &
+cd ${DIR}/MEC
+killall mec-app
+echo ./mec-app ${MEC_OPTS}
+./mec-app osckontrol.json >/tmp/mec.log 2>&1 &
+ps auxwwww | grep mother/mother.pd
+ps auxwwww | grep ${MEC_OPTS}
+
--- scripts/create_macos.sh
+++ scripts/create_macos.sh
@@ -1,6 +1,8 @@
 #/bin/sh
-mkdir -p pkg/macos
-cd pkg/macos
+cd `dirname $0`/..
+DIR=`pwd -P`
+mkdir -p ${DIR}/pkg/macos
+cd ${DIR}/pkg/macos
 cp -R ../../Core/* .
 cp -R ../../macOS/* .
 
