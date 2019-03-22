5a0d660d66501df8e5276c73793df78c1c47daee
Mark Harris <mharris@technobear.com>
2019-03-22 20:30:23 +0100

    initial mpe/ctrl/mfx

--- macOS/run.sh
+++ macOS/run.sh
@@ -7,12 +7,12 @@ export MEC_OPTS=${MEC_OPTS:="osckontrol.json"}
 #PD-OPTS='-nogui -rt - audiobuf 4'
 killall pd
 cd ${DIR}/orac
-echo ${PD} ${PD_OPTS} mother/mother.pd main.pd
-${PD} ${PD_OPTS} mother/mother.pd main.pd >/tmp/orac.log 2>&1 &
+echo ${PD} ${PD_OPTS} mother.pd main.pd
+${PD} ${PD_OPTS} mother.pd main.pd >/tmp/orac.log 2>&1 &
 cd ${DIR}/MEC
 killall mec-app
 echo ./mec-app ${MEC_OPTS}
 ./mec-app ${MEC_OPTS}  >/tmp/mec.log 2>&1 &
-ps auxwwww | grep mother/mother.pd
+ps auxwwww | grep mother.pd
 ps auxwwww | grep ${MEC_OPTS}
 
