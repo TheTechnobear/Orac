04de92cb40396c3da93892b4e0e0ef46d9b04997
Mark Harris <mharris@technobear.com>
2019-04-05 00:43:13 +0200

    nebulae is arch linux

--- scripts/create_nebulae.sh
+++ scripts/create_nebulae.sh
@@ -8,38 +8,28 @@ cd pkg/nebulae
 cp -R ../../Core/* .
 cp -R ../../Bela/* .
 
+mkdir mec_arch 
+cd mec_arch 
+cp ../packaging/mec_arch/PKGBUILD
+cp ../packaging/mec_arch/mec.install
 
-
-mkdir mec_deb 
-cd mec_deb 
-mkdir DEBIAN
-cp -R ../packaging/mec_pkg/* DEBIAN
 mkdir -p etc/systemd/system
 cp ../MEC/mec.service etc/systemd/system
 mkdir -p etc/udev/rules.d/
 cp ../packaging/*.rules etc/udev/rules.d/
 mkdir -p usr/local/
 cp -R ../MEC usr/local/
+makepkg
 cd ..
-fakeroot dpkg --build mec_deb
-mv mec_deb.deb mec.deb
-rm -rf mec_deb
 
 
-mkdir orac_deb 
-cd orac_deb 
-mkdir DEBIAN
-cp -R ../packaging/orac_pkg/* DEBIAN
+mkdir orac_arch 
+cd orac_arch 
+cp ../packaging/orac_arch/PKGBUILD
+cp ../packaging/orac_arch/orac.install
 #mkdir -p etc/systemd/system
 #cp ../orac/orac.service etc/systemd/system
 mkdir -p usr/local/
 cp -R ../orac usr/local/
+makepkg
 cd ..
-fakeroot dpkg --build orac_deb
-mv orac_deb.deb orac.deb
-rm -rf orac_deb
-
-
-
-
-
