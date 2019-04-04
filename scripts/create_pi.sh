a0b07c12ed984fe748e84917ae2112a52192c81d
Mark Harris <mharris@technobear.com>
2019-04-04 20:13:02 +0200

    initial deb packaging

--- scripts/create_pi.sh
+++ scripts/create_pi.sh
@@ -7,4 +7,35 @@ cd pkg/pi
 cp -R ../../Core/* .
 cp -R ../../PI/* .
 
-tar cvzf orac.tgz * 
+#now create debian packages
+mkdir mec_deb 
+cd mec_deb 
+mkdir DEBIAN
+cp -R ../packaging/mec_pkg/* DEBIAN
+mkdir -p etc
+cp ../packaging/amidiauto.conf etc
+mkdir -p etc/systemd/system
+cp ../MEC/mec.service etc/systemd/system
+mkdir -p etc/udev/rules.d/
+cp ../packaging/*.rules etc/udev/rules.d/
+mkdir -p usr/local/
+cp -R ../MEC usr/local/
+cd ..
+fakeroot dpkg --build mec_deb
+mv mec_deb.deb mec.deb
+rm -rf mec_deb
+
+
+mkdir orac_deb 
+cd orac_deb 
+mkdir DEBIAN
+cp -R ../packaging/orac_pkg/* DEBIAN
+mkdir -p etc/systemd/system
+cp ../Orac/orac.service etc/systemd/system
+mkdir -p usr/local/
+cp -R ../Orac usr/local/
+cd ..
+fakeroot dpkg --build orac_deb
+mv orac_deb.deb orac.deb
+rm -rf orac_deb
+
