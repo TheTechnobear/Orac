513aa5aa70d32e4fcced03af0e478caf8a663889
Mark Harris <mharris@technobear.com>
2019-04-04 22:16:03 +0200

    change bela and nebulae to use deb packages

--- scripts/create_bela.sh
+++ scripts/create_bela.sh
@@ -6,3 +6,37 @@ cd pkg/bela
 cp -R ../../Core/* .
 cp -R ../../Bela/* .
 
+
+#now create debian packages
+mkdir mec_deb 
+cd mec_deb 
+mkdir DEBIAN
+cp -R ../packaging/mec_pkg/* DEBIAN
+mkdir -p etc/systemd/system
+cp ../MEC/mec.service etc/systemd/system
+mkdir -p etc/udev/rules.d/
+cp ../packaging/*.rules etc/udev/rules.d/
+mkdir -p usr/local/
+cp -R ../MEC usr/local/
+cd ..
+fakeroot dpkg --build mec_deb
+mv mec_deb.deb mec.deb
+rm -rf mec_deb
+
+
+#bela we dont run as orac as systemd service
+mkdir orac_deb 
+cd orac_deb 
+mkdir DEBIAN
+cp -R ../packaging/orac_pkg/* DEBIAN
+#mkdir -p etc/systemd/system
+#cp ../orac/orac.service etc/systemd/system
+mkdir -p usr/local/
+cp -R ../orac usr/local/
+cd ..
+fakeroot dpkg --build orac_deb
+mv orac_deb.deb orac.deb
+rm -rf orac_deb
+
+
+
--- scripts/create_nebulae.sh
+++ scripts/create_nebulae.sh
@@ -5,13 +5,41 @@ mkdir -p pkg/nebulae
 mkdir -p pkg/nebulae/pd
 
 cd pkg/nebulae
-cp ../../Nebulae/deploy.sh .
-cp ../../Nebulae/pd/* pd 
-cp -R ../../Core/orac pd
-cp -R ../../PI/orac pd
-cp -R ../../Nebulae/orac pd
+cp -R ../../Core/* .
+cp -R ../../Bela/* .
 
-#cp -R ../../Core/MEC/* .
-cp -R ../../PI/MEC .
-cp -R ../../Nebulae/MEC/* MEC
+
+
+mkdir mec_deb 
+cd mec_deb 
+mkdir DEBIAN
+cp -R ../packaging/mec_pkg/* DEBIAN
+mkdir -p etc/systemd/system
+cp ../MEC/mec.service etc/systemd/system
+mkdir -p etc/udev/rules.d/
+cp ../packaging/*.rules etc/udev/rules.d/
+mkdir -p usr/local/
+cp -R ../MEC usr/local/
+cd ..
+fakeroot dpkg --build mec_deb
+mv mec_deb.deb mec.deb
+rm -rf mec_deb
+
+
+mkdir orac_deb 
+cd orac_deb 
+mkdir DEBIAN
+cp -R ../packaging/orac_pkg/* DEBIAN
+#mkdir -p etc/systemd/system
+#cp ../orac/orac.service etc/systemd/system
+mkdir -p usr/local/
+cp -R ../orac usr/local/
+cd ..
+fakeroot dpkg --build orac_deb
+mv orac_deb.deb orac.deb
+rm -rf orac_deb
+
+
+
+
 
