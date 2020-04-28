a46fd4d07fd0fd0fe5aa2c8e9574dcf3623b2661
Mark Harris <mharris@technobear.com>
2020-04-28 16:36:25 +0200

    update norns for sidekick etc

--- scripts/copy_sidekick.sh
+++ scripts/copy_sidekick.sh
@@ -2,20 +2,19 @@
 ARG=`echo $1 | tr -d '/' `
 sidekick_DIR=$ARG/sidekick
 
-if [ "$ARG" != "Norns" ] 
+if [ "$ARG" == "Norns" ] 
 then
-	exit -1
+    mkdir -p ${sidekick_DIR}
+    mkdir -p ${sidekick_DIR}/patches
+    mkdir -p ${sidekick_DIR}/pdexternals
+    mkdir -p ${sidekick_DIR}/nuisdk
+    cp -rf ../NuiLite/resources/sidekick/* ${sidekick_DIR}
+    cp ../NuiLite/build/release/bin/sidekick ${sidekick_DIR}
+    cp ../NuiLite/build/release/lib/*so ${sidekick_DIR}
+    cp ../NuiLite/build/release/lib/* ${sidekick_DIR}/nuisdk
+    cp ../NuiLite/nuilite/NuiDevice.h ${sidekick_DIR}/nuisdk
+    cp -rf ../NuiLite/demos/ ${sidekick_DIR}/patches
+    cp ../NuiLite/build/release/lib/* ${sidekick_DIR}/pdexternals
 fi
 
-mkdir -p ${sidekick_DIR}
-mkdir -p ${sidekick_DIR}/patches
-mkdir -p ${sidekick_DIR}/pdexternals
-mkdir -p ${sidekick_DIR}/nuisdk
-cp -rf ../NuiLite/resources/sidekick/* ${sidekick_DIR}
-cp ../NuiLite/build/release/bin/sidekick ${sidekick_DIR}
-cp ../NuiLite/build/release/lib/*so ${sidekick_DIR}
-cp ../NuiLite/build/release/lib/* ${sidekick_DIR}/nuisdk
-cp ../NuiLite/nuilite/NuiDevice.h ${sidekick_DIR}/nuisdk
-cp -rf ../NuiLite/demos/ ${sidekick_DIR}/patches
-cp ../NuiLite/build/release/lib/* ${sidekick_DIR}/pdexternals
 
